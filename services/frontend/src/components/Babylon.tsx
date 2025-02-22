b6c238353038f928a92a6cbd4897045cc7ec0739
Bastien Wisniewski <wisniewski.bastien31@gmail.com>
2025-02-22 16:38:28 +0100

    Move Babylon in a directory

--- services/frontend/src/components/Babylon.tsx
+++
@@ -1,8 +0,0 @@
-import Babact from "babact"
-
-export default function Babylon() {
-	// return <div style="width: 100vw; height: 100vh; position: absolute; top: 0; left: 0; background-color: gray;">
-	return <div style="width: 100vw; height: 100vh; position: absolute; top: 0; left: 0; background-image: url(https://st5.depositphotos.com/23188010/77062/i/450/depositphotos_770624600-stock-photo-green-field-morning-render-illustration.jpg); background-size: cover; background-position: center;">
-
-	</div>
-}
