de0d7087c79da1f3081a3ef15f4eafe31fde4026
Bastien Wisniewski <46350587+UnRenardQuiDab@users.noreply.github.com>
2025-03-08 16:05:39 +0100

    Add auto refresh on fetch wrapper.

--- services/frontend/src/hooks/useFetch.ts
+++ services/frontend/src/hooks/useFetch.ts
@@ -1,10 +1,30 @@
 import Babact from "babact";
 import useToast from "./useToast";
+import config from "../config";
 
 export default function useFetch() {
 
 	const {createToast} = useToast();
 	const [isLoading, setIsLoading] = Babact.useState(false);
+
+	const refreshToken = async () => {
+		const expired_at = localStorage.getItem('expire_at');
+		if (!expired_at)
+			return;
+		const expired_at_date = new Date(expired_at);
+		if (expired_at_date > new Date())
+			return;
+		const response = await ft_fetch(`${config.API_URL}/token/refresh`, {
+			method: 'POST',
+			credentials: 'include'
+		}, {
+			disable_bearer: true
+		});
+		if (response) {
+			localStorage.setItem('access_token', response.access_token);
+			localStorage.setItem('expire_at', response.expire_at);
+		}
+	};
 
 	const ft_fetch = async (
 		url: string,
@@ -17,9 +37,11 @@ export default function useFetch() {
 		} = {}
 	) => {
 		setIsLoading(true);
-		const token = localStorage.getItem('access_token');
 		try {
+			if (!option.disable_bearer)
+				await refreshToken();
 			const headers = fetch_options?.headers || {};
+			const token = localStorage.getItem('access_token');
 			if (!option.disable_bearer && token) {
 				headers['Authorization'] = `Bearer ${token}`;
 			}
