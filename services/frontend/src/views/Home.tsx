226372147b8b1bcf3f9d054e9a25fed83f9b987e
Bastien Wisniewski <wisniewski.bastien31@gmail.com>
2025-03-20 18:55:51 +0100

    Restore window close for FortyTwoView and remove forced refresh button

--- services/frontend/src/views/Home.tsx
+++ services/frontend/src/views/Home.tsx
@@ -1,21 +1,10 @@
 import Babact from "babact"
 import Overlay from "../templates/Overlay"
-import Button from "../ui/Button"
-import useFetch from "../hooks/useFetch";
  
 export default function Home() {
 
-	const { forceRefresh } = useFetch();
-
 	return <Overlay>
 		<h1>Home</h1>
-		<div className='flex'>
-			<Button
-				style='margin-top: 5rem;'
-				onClick={() => forceRefresh()}
-			>
-				Refresh
-			</Button>
-		</div>
+
 	</Overlay>
 }
