95758a18a63e1eb2c797a91478f983b2dfcb87e4
Ofacy <github@ofacy.com>
2025-03-27 11:55:08 +0100

    feat!: begin work on game creation logic

--- modules/pong/src/core/Pong.ts
+++ modules/pong/src/core/Pong.ts
@@ -1,26 +1,47 @@
 import * as PH2D from "physics-engine";
 import { Vec2 } from "gl-matrix";
-import { GameMode } from "./types.js";
+import { GameMode, IPlayer } from 'yatt-lobbies'
 import { DT, ballSpeedMin, bounceMaterial } from "./constants.js";
 
+export enum PongState {
+	RESERVED = "reserved",
+	PLAYING = "playing",
+	PAUSED = "paused",
+	ENDED = "ended"
+}
+
 export class Pong {
-	private _counter: number = 0;
+	private _matchId: number;
+	private _physicsScene: PH2D.Scene;
+	private _gameMode: GameMode;
+	private _players: IPlayer[] = [];
+	private _state: PongState = PongState.RESERVED;
 
-	public constructor() {}
+	private _balls: PH2D.Body[] = [];
+	private _paddles: Map<number, PH2D.Body> = new Map();
 
-	public incrementCounter(): void {
-		this._counter++;
+	public constructor(match_id: number, gamemode: GameMode, players: IPlayer[], state: PongState = PongState.RESERVED) {
+		this._matchId = match_id;
+		this._gameMode = gamemode;
+		this._players = players;
+		this._physicsScene = new PH2D.Scene(Vec2.create(), DT);
+		this._state = state;
 	}
 
-	public decrementCounter(): void {
-		this._counter--;
+	public toJSON() {
+		return {
+			players: this._players,
+			match_id: this._matchId,
+			gamemode: this._gameMode
+		};
 	}
 
-	public get counter(): number {
-		return this._counter;
+	public shouldUpdate(): boolean {
+		return true;
 	}
 
-	public set counter(value: number) {	// imagine this setter has some validation logic
-		this._counter = value;
+	public update() {
+		this._physicsScene.step();
 	}
 }
+
--- services/pong/srcs/routes/root.js
+++ services/pong/srcs/routes/root.js
@@ -3,22 +3,7 @@
 import { PongServer } from "../PongServer.js";
 import { gameEvents } from "../gameevents.js";
 
-const pong = new PongServer();
-
 export default function router(fastify, opts, done) {
-
-  fastify.get("/", { websocket: true }, async (socket, req) => {
-    console.log("Client connected");
-    socket.send(JSON.stringify({ event: "state", data: { counter: pong.counter } }));
-    pong.addClient(socket);
-    socket.on("message", (message) => {
-      gameEvents.receive(socket, JSON.parse(message), pong);
-    });
-    socket.on("close", () => {
-      pong.removeClient(socket);
-      console.log("Client disconnected");
-    })
-  });
 
   done();
 }
