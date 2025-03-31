514b544e30c47a06250afe7af207260244c87d22
ibertran <128724498+ibtrd@users.noreply.github.com>
2025-03-31 22:15:48 +0200

    fix: fix social bad envar name for social service

--- services/social/srcs/app/env.js
+++ services/social/srcs/app/env.js
@@ -4,6 +4,6 @@ if (!jwt_secret) {
   process.exit(1);
 }
 
-export const offline_delay = process.env.SOCIAL_INACTIVITY_DELAY || 30 * 1000 // Defaults to 30sec
+export const offline_delay = process.env.SOCIAL_OFFLINE_DELAY || 30 * 1000 // Defaults to 30sec
 
 export const inactivity_delay = process.env.SOCIAL_INACTIVITY_DELAY || 2 * 60 * 1000 // Defaults to 2min
