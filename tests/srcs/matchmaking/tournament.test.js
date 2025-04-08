a5a413d78372bc055ee135c5a726bb7436007d60
Ofacy <github@ofacy.com>
2025-04-08 19:06:00 +0200

    test: rewrite tournament tests to include more and better checks

--- tests/srcs/matchmaking/tournament.test.js
+++ tests/srcs/matchmaking/tournament.test.js
@@ -2,7 +2,7 @@ import request from "superwstest";
 import { matchmaking_jwt_secret } from "./env";
 import { GameModes, matchmakingURL } from "./gamemodes";
 import { createUsers, users } from "../../dummy/dummy-account";
-import {createEventSource} from 'eventsource-client'
+import { createEventSource } from 'eventsource-client'
 
 import Fastify from "fastify";
 import jwt from "@fastify/jwt";
@@ -29,42 +29,100 @@ function finishMatch(match_id, winner) {
     }).expect(200);
 }
 
-function expectSSEEvent(sse, eventName, callback) {
-  return new Promise(async (resolve, reject) => {
-    const event = (await sse[Symbol.asyncIterator]().next()).value;
-    expect(event.event).toBe(eventName);
-    if (callback) {
-      await callback(JSON.parse(event.data));
+function createTestSSE(url) {
+  const messages = [];
+  let closeCallback;
+
+  const object = {
+    messages,
+    sse: createEventSource({
+      url,
+      onMessage: (message) => {
+        messages.push(message);
+      },
+      onDisconnect() {
+        expect(messages.length).toBe(0);
+        if (closeCallback)
+          closeCallback();
+        closeCallback = null;
+        object.close();
+      }
+    }),
+    close() {
+      this.sse.close();
+    },
+    expectJson(eventName, callback) {
+      return new Promise(async (resolve) => {
+        let event = this.messages.shift();
+        if (!event) {
+          for await (event of this.sse)
+            break;
+          this.messages.shift();
+        }
+        expect(event.event).toBe(eventName);
+        const data = JSON.parse(event.data);
+        if (callback)
+          await callback(data);
+        resolve(data);
+      });
+    },
+    expectClose() {
+      return new Promise((resolve) => {
+        closeCallback = resolve;
+      });
     }
-    resolve(event);
-  });
+  };
+  return object;
 }
 
-describe("direct matchmaking", () => {
-  let ws;
-  it("connect to matchmaking websocket", async () => {
-    ws = request(matchmakingURL)
-      .ws("/lobbieconnection")
-      .sendJson({
-        event: "handshake",
-        data: {
-          jwt: app.jwt.sign({ GameModes }),
-        },
+let ws;
+
+it("connect to matchmaking websocket", async () => {
+  ws = request(matchmakingURL)
+    .ws("/lobbieconnection")
+    .sendJson({
+      event: "handshake",
+      data: {
+        jwt: app.jwt.sign({ GameModes }),
+      },
+    })
+    .expectJson((message) => {
+      expect(message.event).toBe("handshake");
+    });
+});
+
+describe.each(
+  Array.from(
+    { length: 14 },
+    (_, index) => ({
+      player_count: index + 3,
+      gamemode: "custom_1v1",
+      team_size: 1
+    })
+  ).concat(
+    Array.from(
+      { length: 28 },
+      (_, index) => ({
+        player_count: index + 5,
+        gamemode: "custom_2v2",
+        team_size: 2
       })
-      .expectJson((message) => {
-        expect(message.event).toBe("handshake");
-      });
-  });
-  it.each(Array.from({ length: 10 }, (_, index) => index + 3))
-  ("queue %d players in a 1v1 custom lobby", async (player_count) => {
+    )
+  ))
+("$gamemode with $player_count players", ({player_count, gamemode, team_size}) => {
+  let team_count = Math.ceil(player_count / team_size);
+  let tournament;
+  let sse;
+  it("create lobby and queue for tournament", async () => {
     const lobby = {
       team_names: [],
-      players: Array.from({ length: player_count }, (_, index) => ({ account_id: users[index].account_id })),
-      mode: GameModes["custom_1v1"],
-      join_secret: `custom_1v1_${player_count}`,
+      players: Array.from({ length: player_count }, (_, index) => ({
+        account_id: users[index].account_id
+      })),
+      mode: GameModes[gamemode],
+      join_secret: `${gamemode}_${player_count}`,
     };
-    let tournament;
-    let sse;
+    
     await ws
       .sendJson({
         event: "queue_lobby",
@@ -75,56 +133,101 @@ describe("direct matchmaking", () => {
         expect(message.data.queue_stats.players).toBe(player_count);
         expect(message.data.queue_stats.lobbies).toBe(1);
       })
-      .expectJson((message) => {
-        expect(message.event).toBe("match");
-        tournament = message.data.match.tournament;
-        expect(tournament).toBeDefined();
-        expect(tournament.id).toBeDefined();
-        expect(tournament.teams.length).toBe(player_count);
-        sse = createEventSource(
-          `${matchmakingURL}/tournaments/${tournament.id}/notify?token=${users[0].jwt}`
-        );
-      });
-    await expectSSEEvent(sse, "sync", (event) => {
+  })
+  it("expect tournament start", async () => {
+    await ws.expectJson((message) => {
+      expect(message.event).toBe("match");
+      expect(message.data.match.tournament).toBeDefined();
+      expect(message.data.match.tournament.id).toBeDefined();
+      expect(message.data.match.tournament.teams.length).toBe(team_count);
+      
+      tournament = message.data.match.tournament;
+    });
+  });
+  it("connect to SSE and expect tournament sync", async () => {
+    sse = createTestSSE(`${matchmakingURL}/tournaments/${tournament.id}/notify?token=${users[0].jwt}`,);
+    await sse.expectJson("sync", (event) => {
       expect(event).toBeDefined();
       expect(event.teams).toBeDefined();
       expect(event.matches).toBeDefined();
-      expect(event.matches.length).toBeLessThanOrEqual(Math.ceil(Math.log2(player_count)));
+      expect(event.matches.length).toBeGreaterThanOrEqual(Math.ceil(Math.log2(team_count)));
       expect(event.gamemode).toBeDefined();
       expect(event.id).toBeDefined();
-      expect(event.teams.length).toBe(player_count);
+      expect(tournament.teams.length).toBe(team_count);
     });
-    for (let match of tournament.matches.filter((match) => match.match_id)) {
-      await finishMatch(match.match_id, 1);
-      await expectSSEEvent(sse, "match_update", (event) => {
-        console.log(event);
-        expect(event.match).toBeDefined();
-        expect(event.match.match_id).toBe(match.match_id);
-        expect(event.match.state).toBe('done');
-        expect(event.match.scores[0]).toBe(0);
-        expect(event.match.scores[1]).toBe(1);
-      });
-      if (match.match_id === tournament.matches[0].match_id) {
-        await expectSSEEvent(sse, "finish", (event) => {
-          console.log(event);
-          expect(event.match).toBeDefined();
-          expect(event.match.state).toBe('playing');
-          expect(event.match.scores[0]).toBe(0);
-          expect(event.match.scores[1]).toBe(0);
-        });
-        return;
-      }
-      await expectSSEEvent(sse, "match_update", (event) => {
-        console.log(event);
-        expect(event.match).toBeDefined();
-        expect(event.match.state).toBe('playing');
-        expect(event.match.scores[0]).toBe(0);
-        expect(event.match.scores[1]).toBe(0);
-      });
-    }
-    sse.close();
-  })
-  it("close matchmaking websocket", async () => {
-    await ws.close();
   });
+  const stages = [];
+  it("build tournament stages", () => {
+    for (let match of tournament.matches) {
+      if (stages[match.stage])
+        stages[match.stage].push(match);
+      else
+        stages[match.stage] = [match];
+    }
+    stages.reverse();
+  });
+  let isFinished = false;
+  it.each(Array.from({ length: Math.ceil(Math.log2(team_count)) }, (_, index) => index))
+    ("complete tournament stage %i", async (stageIndex) => {
+      const stage = stages[stageIndex];
+      for (let matchIndex = 0; matchIndex < stage.length; matchIndex++) {
+        const winnerIndex = 0;
+        const loserIndex = 1;
+        const match = stage[matchIndex];
+
+        expect(match.state).toBe("playing");
+        expect(match.match_id).toBeDefined();
+        expect(match.team_ids.length).toBe(2);
+        await finishMatch(match.match_id, winnerIndex);
+        await sse.expectJson("match_update", (event) => {
+          expect(event.match.match_id).toBe(match.match_id);
+          expect(event.match.index).toBe(match.index);
+          expect(event.match.state).toBe("done");
+          expect(event.match.scores[winnerIndex]).toBe(1);
+          expect(event.match.scores[loserIndex]).toBe(0);
+
+          match.state = event.match.state;
+          match.scores = event.match.scores;
+          match.team_ids = event.match.team_ids;
+          match.match_id = event.match.match_id;
+        });
+        if (match.stage == 0) {
+          await sse.expectJson("finish", async () => {
+            isFinished = true;
+            await sse.expectClose();
+          });
+          break;
+        }
+        const next_stage_match = stages[stageIndex + 1][Math.floor(matchIndex / 2)];
+        const next_event_start = matchIndex % 2 == 1 || next_stage_match.team_ids.length == 1;
+        await sse.expectJson("match_update", (event) => {
+          expect(event.match.index).toBe(next_stage_match.index);
+          if (next_event_start) {
+            expect(event.match.match_id).toBeDefined();
+            expect(event.match.state).toBe("playing");
+            expect(event.match.team_ids.length).toBe(2);
+            expect(event.match.team_ids[1]).toBe(match.team_ids[winnerIndex]);
+          }
+          else {
+            expect(event.match.match_id).toBeUndefined();
+            expect(event.match.state).toBe("waiting");
+            expect(event.match.team_ids.length).toBe(1);
+            expect(event.match.team_ids[0]).toBe(match.team_ids[winnerIndex]);
+          }
+
+          const match_update = tournament.matches[event.match.index];
+          match_update.match_id = event.match.match_id;
+          match_update.state = event.match.state;
+          match_update.team_ids = event.match.team_ids;
+          match_update.scores = event.match.scores;
+        });
+      }
+  });
+  it("expect tournament finished", async () => {
+    expect(isFinished).toBe(true);
+  });
+});
+
+it("close matchmaking websocket", async () => {
+  await ws.close();
 });
