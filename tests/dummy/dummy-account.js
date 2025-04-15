902139024033a5ace7f165ccb8e9ea113654a75b
Ofacy <github@ofacy.com>
2025-04-15 18:46:59 +0200

    test: change dummy-account creation to be made in 'beforeAll'

--- tests/dummy/dummy-account.js
+++ tests/dummy/dummy-account.js
@@ -17,11 +17,9 @@ const registerUrL = "http://127.0.0.1:4012";
 const authUrl = "http://127.0.0.1:4022";
 const dbprofilesUrL = "http://127.0.0.1:7001"
 
-beforeAll(async () => {
-  await app.ready();
-});
-
 export let users = [];
+
+const CREATION_TIMEOUT = 60000;
 
 export function createUsers(count=10) {
   afterAll(async () => {
@@ -33,67 +31,51 @@ export function createUsers(count=10) {
     }
   });
   const USER_COUNT = count;
-  describe(`create ${USER_COUNT} dummy accounts`, () => {
-    it("create dummy accounts", async () => {
-      for (let i = 0; i < USER_COUNT; i++) {
-        const dummy = {
-          account_id: null,
-          jwt: null,
-          email: `dummy-account.${crypto.randomBytes(10).toString("hex")}@jest.com`,
-          password: crypto.randomBytes(4).toString("hex"),
-          username: `DUMMY-${crypto.randomBytes(4).toString("hex")}`,
-        };
-        users.push(request(registerUrL)
-          .post("/")
-          .send({
-            email: dummy.email,
-            password: dummy.password,
-          })
-          .expect(201)
-          .expect("Content-Type", /json/)
-          .then((response) => {
-            dummy.jwt = response.body.access_token;
-            dummy.account_id = app.jwt.decode(response.body.access_token).account_id;
-            return dummy;
-          })
-        )
-      }
-      users = await Promise.all(users);
-    });
-    it("auth using password", async () => {
-      users = await Promise.all(users.map(async (user) => {
-        const response = await request(authUrl)
-          .post("/")
-          .send({
-            email: user.email,
-            password: user.password,
-          })
-          .expect(200)
-          .expect("Content-Type", /json/);
-  
-        user.jwt = response.body.access_token;
-        return user;
-      }));
-    });
-    it("patch username", async () => {
-      users = await Promise.all(users.map(async (user) => {
-        const response = await request(dbprofilesUrL)
-          .patch(`/${user.account_id}`)
-          .send({ username: user.username })
-          .set('Authorization', `Bearer ${user.jwt}`)
-          .expect(200)
-          .expect("Content-Type", /json/);
-  
-        expect(response.body).toEqual(
-          expect.objectContaining({
-            account_id: user.account_id,
-            avatar: expect.any(String),
-            username: user.username,
-          })
-        );
-        return user;
-      }));
-    });
-  })
-  return users;
+  beforeAll(async () => {
+    await app.ready();
+    const create_account = async (dummy) => {
+      let response = await request(registerUrL)
+        .post("/")
+        .send({
+          email: dummy.email,
+          password: dummy.password,
+        })
+        .expect(201)
+        .expect("Content-Type", /json/);
+      
+      dummy.jwt = response.body.access_token;
+      dummy.account_id = app.jwt.decode(response.body.access_token).account_id;
+
+      response = await request(authUrl)
+        .post("/")
+        .send({
+          email: dummy.email,
+          password: dummy.password,
+        })
+        .expect(200)
+        .expect("Content-Type", /json/);
+      dummy.jwt = response.body.access_token;
+
+      response = await request(dbprofilesUrL)
+        .patch(`/${dummy.account_id}`)
+        .send({ username: dummy.username })
+        .set('Authorization', `Bearer ${dummy.jwt}`)
+        .expect(200)
+        .expect("Content-Type", /json/);
+      dummy.profile = response.body;
+      return dummy;
+    };
+    for (let i = 0; i < USER_COUNT; i++) {
+      const dummy = {
+        account_id: null,
+        jwt: null,
+        email: `dummy-account.${crypto.randomBytes(10).toString("hex")}@jest.com`,
+        password: crypto.randomBytes(4).toString("hex"),
+        username: `DUMMY-${crypto.randomBytes(4).toString("hex")}`,
+      };
+      await create_account(dummy);
+      users.push(dummy);
+    }
+    
+  }, CREATION_TIMEOUT);
 }
