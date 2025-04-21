c2028dca4fbdbbb90ba46e7e18b36743e4d012ab
kalde108 <29688104+kalde108@users.noreply.github.com>
2025-04-21 19:43:55 +0200

    chore: remove unecessary method

--- services/frontend/src/components/Babylon/PongClient.ts
+++ services/frontend/src/components/Babylon/PongClient.ts
@@ -164,12 +164,6 @@ export default class PongClient extends PONG.Pong {
 		this._running = 1;
 		this._babylonScene.clearColor = Color4.FromColor3(Color3.Gray());
 	}
-
-	public resetGame() {
-		this._running = 0;
-		this._time = 0;
-		this._score = [0, 0];
-	}
 	
 	private loop = () => {
 		this.update();
