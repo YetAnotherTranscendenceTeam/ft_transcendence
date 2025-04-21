52f5e46fc276551ec203d39adb0d3a7d714a9880
Ofacy <github@ofacy.com>
2025-04-21 23:03:09 +0200

    feat: add elo update

--- services/matchmaking/srcs/routes/protected/matches.js
+++ services/matchmaking/srcs/routes/protected/matches.js
@@ -1,6 +1,8 @@
 import YATT, { HttpError } from "yatt-utils";
 import db from "../../app/database.js";
 import { MatchState } from "../../Match.js";
+import { GameModes } from "../../GameModes.js";
+import { GameModeType } from "yatt-lobbies";
 
 export default function router(fastify, opts, done) {
   fastify.patch(
@@ -28,24 +30,49 @@ export default function router(fastify, opts, done) {
       if (!setClause) new HttpError.BadRequest().send(reply);
 
       const updated = db
-        .prepare(
-          `
-			UPDATE matches
-			SET
+      .prepare(
+        `
+        UPDATE matches
+        SET
 			  ${setClause}
-			WHERE match_id = ?
-			RETURNING *
-		  `
-        )
-        .get(...params, request.params.match_id);
+        WHERE match_id = ?
+        RETURNING *
+        `
+      )
+      .get(...params, request.params.match_id);
       if (!updated) {
         return new HttpError.NotFound().send(reply);
       }
       const tournamentMatch = fastify.tournaments.getTournamentMatch(request.params.match_id);
       if (tournamentMatch)
         tournamentMatch.updateMatch(request.body);
-      if (request.body.state === MatchState.DONE) {
-        // TODO: update player ELO
+      if (request.body.state === MatchState.DONE && GameModes[updated.gamemode].type === GameModeType.RANKED) {
+        const winning_team = updated.score_0 > updated.score_1 ? 0 : 1;
+        const updated_elo = db.prepare(`
+          UPDATE matchmaking_users
+            SET
+              elo = elo
+                + ((
+                  CASE WHEN match_players.team_index = @winning_team THEN
+                    2
+                  ELSE
+                    0
+                  END
+                ) - match_players.win_probability)
+                * (15
+                * (10 - (SELECT MIN(10, matchmaking_users.match_count))) * 0.6),
+              match_count = match_count + 1
+          FROM match_players
+          WHERE
+            match_players.match_id = ?
+            AND match_players.account_id = matchmaking_users.account_id
+            AND matchmaking_users.gamemode = ?
+          RETURNING 
+            matchmaking_users.account_id,
+            matchmaking_users.elo,
+            matchmaking_users.match_count,
+            matchmaking_users.updated_at
+        `).all({winning_team}, request.params.match_id, updated.gamemode);
       }
       reply.send(updated);
     }
