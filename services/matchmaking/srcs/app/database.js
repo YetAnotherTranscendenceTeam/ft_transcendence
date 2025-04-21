28d38400f22f4f523c64d4bf5fab1ddc8f97545c
Ofacy <github@ofacy.com>
2025-04-21 23:02:04 +0200

    feat: add win probability

--- services/matchmaking/srcs/app/database.js
+++ services/matchmaking/srcs/app/database.js
@@ -10,6 +10,7 @@ db.exec(`
     account_id INTEGER NOT NULL,
     gamemode TEXT NOT NULL,
     elo INTEGER NOT NULL,
+    match_count INTEGER NOT NULL DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(account_id, gamemode)
@@ -51,11 +52,14 @@ db.exec(`
     match_id INTEGER NOT NULL,
     account_id INTEGER NOT NULL,
     team_index INTEGER NOT NULL,
+    player_index INTEGER NOT NULL,
+    win_probability REAL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT,
     PRIMARY KEY (match_id, account_id),
     FOREIGN KEY (match_id) REFERENCES matches(match_id),
-    UNIQUE (match_id, account_id)
+    UNIQUE (match_id, account_id),
+    UNIQUE (match_id, team_index, player_index)
   )`);
 
 db.exec(`
