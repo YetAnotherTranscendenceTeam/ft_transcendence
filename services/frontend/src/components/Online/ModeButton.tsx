fdb61b6e8709ef067b02f7cabd7ecb929a192de3
Bastien Wisniewski <46350587+UnRenardQuiDab@users.noreply.github.com>
2025-04-24 16:10:29 +0200

    Add Select mode description

--- services/frontend/src/components/Online/ModeButton.tsx
+++ services/frontend/src/components/Online/ModeButton.tsx
@@ -1,6 +1,6 @@
 import Babact from "babact";
 import { useLobby } from "../../contexts/useLobby";
-import { GameMode } from "yatt-lobbies";
+import { GameMode, GameModeType } from "yatt-lobbies";
 
 export default function ModeButton({
 		gamemode,
@@ -12,13 +12,24 @@ export default function ModeButton({
 
 	const { lobby } = useLobby();
 
+	const getDescription = () => {
+		if (gamemode.type === GameModeType.CUSTOM)
+			return "Up to 16 teams, any rules. 3+ teams? It's tournament time.";
+		if (gamemode.type === GameModeType.RANKED)
+			return 'Competitive 1v1 battles with power-ups enabled. Every match affects your MMR';
+		if (gamemode.type === GameModeType.UNRANKED)
+			return '1v1 matches with power-ups enabled. Play freely without affecting your MMR';
+	}
+
+
 
 	const disabled = (lobby && gamemode.getLobbyCapacity() < lobby.players.length) || lobby?.mode.name === gamemode.name;
 	return <div
-		className={`mode-button flex items-center justify-center ${gamemode.type} ${disabled ? 'disabled' : ''}`}
+		className={`mode-button flex flex-col justify-end gap-2 ${gamemode.type} ${disabled ? 'disabled' : ''}`}
 		onClick={() => onSelect(gamemode.name)}
 	>
-		<h1>{gamemode.getDisplayName()}</h1>
-		<p>{gamemode.type}</p>
+		<h1>{gamemode.type}</h1>
+		<h2>{gamemode.getDisplayName()}</h2>
+		<p>{getDescription()}</p>
 	</div>
 }
