3ef5015710579225965e0a747f1a1d0ba0f500cb
Ofacy <github@ofacy.com>
2025-04-25 13:22:52 +0200

    fix: custom gamemode not allowing queue with 2 teams

--- modules/yatt-lobbies/src/GameMode.ts
+++ modules/yatt-lobbies/src/GameMode.ts
@@ -64,7 +64,7 @@ export class GameMode implements IGameMode {
   }
 
   getLobbyCapacity(): number {
-    return this.type === GameModeType.TOURNAMENT ? this.team_size * this.team_count : this.team_size;
+    return this.type === GameModeType.TOURNAMENT || this.type === GameModeType.CUSTOM ? this.team_size * this.team_count : this.team_size;
   }
 
   getDisplayName(): string {
