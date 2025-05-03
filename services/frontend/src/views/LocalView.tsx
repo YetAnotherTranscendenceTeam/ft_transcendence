200be8ef0bbdd74a9dba0c4b499840b4c761ba4c
Bastien Wisniewski <46350587+UnRenardQuiDab@users.noreply.github.com>
2025-05-03 18:37:28 +0200

    remove deprecated import

--- services/frontend/src/views/LocalView.tsx
+++ services/frontend/src/views/LocalView.tsx
@@ -3,13 +3,12 @@ import { GameScene } from "../components/Babylon/types";
 import { usePong } from "../contexts/usePong";
 import Overlay from "../templates/Overlay";
 import useEscape from "../hooks/useEscape";
-import { IPongState, PongState } from "pong";
+import { PongState } from "pong";
 import GameOverlay from "../components/Game/GameOverlay";
 
 export default function LocalView() {
 
 	const { app, overlay, togglePause, startGame } = usePong();
-	// const [hidden, setHidden] = Babact.useState<boolean>(false);
 
 	Babact.useEffect(() => {
 		app.setGameScene(GameScene.LOCAL);
@@ -29,45 +28,11 @@ export default function LocalView() {
 
 	useEscape(overlay?.gameStatus.name === PongState.PLAYING.name, () => handlePause())
 
-	// const getModal = () => {
-	// 	if (gameStatus === GameStatus.FREEZE)
-	// 		return <NextRoundModal
-	// 			onNextRound={() => {
-	// 				setGameStatus(GameStatus.PLAYING);
-	// 				app.nextRound();
-	// 			}}
-	// 		/>
-	// 	if (gameStatus === GameStatus.WAITING)
-	// 		return <LocalStartModal
-	// 			onClick={() => setHidden(true)}
-	// 			onStart={() => handleStart()}
-	// 		/>
-	// 	if (gameStatus === GameStatus.PAUSED)
-	// 		return <GamePausedModal
-	// 			onClick={() => setHidden(true)}
-	// 			onResume={() => handleResume()}
-	// 		/>
-	// 	if (gameStatus === GameStatus.ENDED)
-	// 		return <WinnerModal
-	// 			onClick={() => setHidden(true)}
-	// 			onPlayAgain={() => {
-	// 				handleStart();
-	// 			}}
-	// 		/>
-	// 	return null;
-	// }
-
-	// Babact.useEffect(() => {
-	// 	if (overlay.gameStatus.name === PongState.ENDED.name) {
-	// 		setHidden(false);
-	// 	}
-	// }, [overlay])
 
 	if (!overlay)
 		return;
 
 	return <Overlay
-		// modal={getModal()}
 		hidden={overlay.gameStatus.name === PongState.PLAYING.name || overlay.gameStatus.name === PongState.FREEZE.name}
 	>
 		<GameOverlay
--- services/frontend/src/views/OnlineView.tsx
+++ services/frontend/src/views/OnlineView.tsx
@@ -1,14 +1,7 @@
 import Babact from "babact";
-import { GameScene } from "../components/Babylon/types";
 import { usePong } from "../contexts/usePong";
-import Scores from "../components/Game/Scores";
-import LocalStartModal from "../components/Game/LocalStartModal";
 import Overlay from "../templates/Overlay";
-import useEscape from "../hooks/useEscape";
-import GamePausedModal from "../components/Game/GamePausedModal";
-import WinnerModal from "../components/Game/WinnerMatchOverlay";
 import { useNavigate, useParams } from "babact-router-dom";
-import OnlineStartModal from "../components/Game/OnlineStartModal";
 import GameOverlay from "../components/Game/GameOverlay";
 import { PongState } from "pong";
 
@@ -16,7 +9,6 @@ export default function OnlineView() {
 
 	const { app } = usePong();
 	const { id } = useParams();
-	const navigate = useNavigate();
 
 	const { overlay } = usePong();
 
@@ -28,7 +20,6 @@ export default function OnlineView() {
 		return;
 	return <Overlay
 		hidden={overlay.gameStatus.name === PongState.PLAYING.name || overlay.gameStatus.name === PongState.FREEZE.name}
-		// modal={getModal()}
 	>
 		<GameOverlay/>
 	</Overlay>
