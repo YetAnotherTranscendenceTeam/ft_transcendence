d0d0fe05ca770317f96e0498cfa2c68c46cd8c9d
kalde108 <29688104+kalde108@users.noreply.github.com>
2025-05-03 22:50:10 +0200

    feat: physics engine polygon now accept counter clockwise vertices

--- modules/physics-engine/src/core/utils.ts
+++ modules/physics-engine/src/core/utils.ts
@@ -3,3 +3,12 @@ import { Vec2 } from "gl-matrix";
 export function pseudoCross(v1: Vec2, v2: Vec2): number {
 	return v1[0] * v2[1] - v1[1] * v2[0];
 }
+
+export function signedArea2(verts: Vec2[]): number {
+	let sum = 0;
+	for (let i = 0; i < verts.length; i++) {
+		const j = (i + 1) % verts.length;
+		sum += verts[i].x * verts[j].y - verts[j].x * verts[i].y;
+	}
+	return sum;
+}
