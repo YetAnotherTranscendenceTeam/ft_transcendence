c6c1438bbd0acc5d41a9c14513d2e8dad2f1eebe
ibertran <128724498+ibtrd@users.noreply.github.com>
2025-05-05 12:46:29 +0200

    fix: cors configuration not being homogeneous

--- modules/yatt-utils/srcs/swagger/setup.js
+++
@@ -1,9 +0,0 @@
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-
-export default async function setUpSwagger(app, openapi) {
-  await app.register(fastifySwagger, { openapi });
-  await app.register(fastifySwaggerUi, {
-    routePrefix: "/api-docs",
-  });
-}
