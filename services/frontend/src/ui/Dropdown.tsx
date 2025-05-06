43e78678e9c3b5497112ccfe9e8a17785c1e4158
Bastien Wisniewski <46350587+UnRenardQuiDab@users.noreply.github.com>
2025-05-06 18:10:55 +0200

    Add custom pos and improve click outside function

--- services/frontend/src/ui/Dropdown.tsx
+++ services/frontend/src/ui/Dropdown.tsx
@@ -6,21 +6,30 @@ export default function Dropdown({
 		openButton,
 		openButtonClassName = '',
 		className = '',
+		pos = 'left',
 		...props
 	}: {
 		children?: any,
 		openButton: any,
 		className?: string,
+		pos?: 'left' | 'right' | 'top' | 'bottom',
 		[key: string]: any
 	}) {
 	
 	const [isOpen, setIsOpen] = Babact.useState<boolean>(false);
+	const dropdownRef = Babact.useRef<HTMLDivElement>(null);
+	const id = Babact.useRef<number>(Math.floor(Math.random() * 1000000));
 
 	const handleClickOutside = (e: MouseEvent) => {
-		if (e.target instanceof HTMLElement && !e.target.closest('.dropdown')) {
+		if (!dropdownRef.current) return;
+		if (e.target instanceof HTMLElement && !dropdownRef.current.contains(e.target)) {
 			setIsOpen(false);
 		}
 	}
+
+	Babact.useEffect(() => {
+		dropdownRef.current = document.querySelector(`#dropdown${id.current}`) as HTMLDivElement;
+	}, [])
 
 	Babact.useEffect(() => {
 		if (!isOpen) return;
@@ -30,14 +39,17 @@ export default function Dropdown({
 		}
 	}, [isOpen]);
 
-	return <div className={`dropdown ${className}`} {...props}>
+
+	return <div className={`dropdown ${className}`} {...props}
+		id={'dropdown' + id.current}
+	>
 		<Button
 			className={`${openButtonClassName}`}
 			onClick={() => setIsOpen(!isOpen)}
 		>
 			{openButton}
 		</Button>
-		<div className={`dropdown-content ${isOpen ? 'open' : ''}`}>
+		<div className={`dropdown-content ${isOpen ? 'open' : ''} ${pos}`}>
 			{children}
 		</div>
 	</div>
