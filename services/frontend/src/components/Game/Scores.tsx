5dc7cf6460b021738210171948299e2d3ef75b00
Bastien Wisniewski <46350587+UnRenardQuiDab@users.noreply.github.com>
2025-05-07 14:27:20 +0200

    change scores overlay

--- services/frontend/src/components/Game/Scores.tsx
+++ services/frontend/src/components/Game/Scores.tsx
@@ -7,46 +7,81 @@ import Avatar from "../../ui/Avatar";
 export default function Scores() {
 
 	const { overlay } = usePong();
+	const [scores, setScores] = Babact.useState<string[]>(null);
+
+	const pointToWin = 5;
+	const bestOf = pointToWin * 2 - 1;
+	const goal = Math.floor(bestOf / 2);
+
+	const generateScores = () => {
+		const newSocres = new Array(overlay.scores[0]).fill('team-1').concat(
+			new Array(bestOf - overlay.scores[0] - overlay.scores[1]).fill('')
+		).concat(
+			new Array(overlay.scores[1]).fill('team-2')
+		);
+		console.log('generateScores', overlay.scores, overlay.lastWinner, newSocres);
+		if (overlay.lastWinner !== null) {
+			if (overlay.lastWinner === 0) {
+				newSocres[overlay.scores[0] - 1] += ' pulse';
+			}
+		}
+		setScores(newSocres);
+	}
+
+	Babact.useEffect(() => {
+		if (overlay.scores) {
+			generateScores();
+		}
+	}, [overlay?.scores[0], overlay?.scores[1], overlay?.lastWinner]);
 
 	return <Card className='scores'>
-		<div className="flex flex-row gap-4 items-center">
+		<div className="flex flex-row gap-2 items-center flex-1">
 			{ overlay.teams.length > 1 &&
 				<div className="flex items-center gap-2">
 					{overlay.teams[0].players.map((player, index) => (
 						<Avatar
-							name={player.profile.username}
-							src={player.profile.avatar}
-							key={player.account_id}
+						name={player.profile.username}
+						src={player.profile.avatar}
+						key={player.account_id}
 						/>
 					))}
-					<p>
-						{overlay.teams[0].getDisplayName()}
-					</p>
 				</div>
 			}
-			<div className='score'>
-				{overlay.scores[0]}
-			</div>
-			<div className='time'>
-				{Math.round(overlay.time / 60).toString().padStart(2, '0')} : {Math.round(overlay.time % 60).toString().padStart(2, '0')}
-			</div>
-			<div className='score'>
-				{overlay.scores[1]}
+			<div className='score flex flex-col gap-2 justify-center items-center'>
+				<div
+					className='scrore-top flex gap-2 justify-center items-center w-full'
+				>
+					{overlay.teams.length > 1 &&<div className="score-team-name flex flex-col">
+						<p>{overlay.teams[0].getDisplayName()}</p>
+						<p>{overlay.teams[1].getDisplayName()}</p>
+					</div>}
+					<i className="fa-solid fa-crown"></i>
+					{overlay.teams.length > 1 &&<div className="score-team-name flex flex-col">
+						<p>{overlay.teams[1].getDisplayName()}</p>
+						<p>{overlay.teams[0].getDisplayName()}</p>
+					</div>}
+				</div>
+				<div className='score-frame-container flex justify-between items-center gap-1'>
+					{scores && scores.map((_, index) => <span
+						className={`score-frame ${index === goal ? 'goal' : ''} ${_}`}
+						key={index}
+					/>)}
+				</div>
 			</div>
 			{ overlay.teams.length > 1 &&
-				<div className="flex items-center gap-2">
-					<p>
-						{overlay.teams[1].getDisplayName()}
-					</p>
+				<div className="flex items-center gap-2 flex-1">
 					{overlay.teams[1].players.map((player, index) => (
 						<Avatar
-							name={player.profile.username}
-							src={player.profile.avatar}
-							key={player.account_id}
+						name={player.profile.username}
+						src={player.profile.avatar}
+						key={player.account_id}
 						/>
 					))}
 				</div>
 			}
+		</div>
+		<div className='time'>
+			{Math.round(overlay.time / 60).toString().padStart(2, '0')} : {Math.round(overlay.time % 60).toString().padStart(2, '0')}
 		</div>
 	</Card>
 }
