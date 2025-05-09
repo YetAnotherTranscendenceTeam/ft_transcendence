2c36f45f3748688e8ca58ab8e478a7794c532fce
Ofacy <github@ofacy.com>
2025-05-09 17:39:55 +0200

    feat: add matchmaking user to users fetch by account_id

--- services/users/srcs/routes/root.js
+++ services/users/srcs/routes/root.js
@@ -55,6 +55,24 @@ export default function router(fastify, opts, done) {
 
     const profile = await YATT.fetch(`http://profiles:3000/${account_id}`);
 
+    try {
+      // Add game related infos
+      const { last_match, last_tournament, matchmaking_users } = await YATT.fetch(`http://matchmaking:3000/users/${account_id}`,
+        {
+          headers: {
+            Authorization: request.headers.authorization,
+          },
+        }
+      );
+
+      profile.last_match = last_match;
+      profile.last_tournament = last_tournament;
+      profile.matchmaking_users = matchmaking_users;
+    } catch (err) {
+      profile.last_match = null;
+      profile.last_tournament = null;
+      profile.matchmaking_users = [];
+    }
     reply.send(profile);
   });
 
