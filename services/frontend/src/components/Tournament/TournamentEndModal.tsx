2f286197a17efbdfb3380577ed4f4d1ab24ae5bf
Bastien Wisniewski <46350587+UnRenardQuiDab@users.noreply.github.com>
2025-05-19 17:34:20 +0200

    fix: ugly tournament winner modal

--- services/frontend/src/components/Tournament/TournamentEndModal.tsx
+++ services/frontend/src/components/Tournament/TournamentEndModal.tsx
@@ -31,14 +31,15 @@ export default function TournamentEndModal({
 	return <Modal
 		isOpen={isOpen}
 		onClose={() => onClose()}
-		className='tournament-end-modal flex flex-col items-center justify-center gap-4'
+		className='tournament-end-modal top flex flex-col items-center justify-center gap-4'
 	>
-		<h2><i className="fa-solid fa-trophy"></i> {winningTeam.getDisplayName()} has won the clash</h2>
+		<h1>Congratulations!</h1>
+		<h2 className='flex gap-4'><i className="fa-solid fa-trophy"></i> {winningTeam.getDisplayName()} <i className="fa-solid fa-trophy"></i></h2>
 		<div className='flex flex-col gap-2'>
 			{winningTeam.players.map((player) => (
 				<div key={player.account_id} className='flex items-center gap-2'>
 					<Avatar src={player.profile.avatar} name={player.profile.username}  />
-					<p>{player.profile.username}</p>
+					{finalMatch.teams[0].players.length > 1 && <p>{player.profile.username}</p>}
 				</div>
 			))}
 		</div>
